20e16c292a72632517176ce5fcad8e9fafedb3e5
AnnetZh <anzhilkina@gmail.com>
2024-12-02 15:21:00 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,15 @@
 def send_email(message,recipient,*sender="university.help@gmail.com"):
-    if "@" not in recipient or "@" not in sender or not (recipient.endswith(".com") or (recipient.endswith(".ru") or (recipient.endswith(".net)) or not (sender.endswith(".com") or (sender.endswith(".ru") or (sender.endswith(".net"")):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    if "@" not in recipient or "@" not in sender:
+       print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif not (recipient.endswith(".com") or (recipient.endswith(".ru") or (recipient.endswith(".net"):
+       print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"))
+    elif not (sender.endswith(".com") or (sender.endswith(".ru") or (sender.endswith(".net")):
+       print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender==recipient:
+        print("Нельзя отправить письмо самому себе!")
+
+    elif sender!="university.help@gmail.com":
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient})
+
     else:
-        send_email(resipient,sender)
+        send_email(resipient,sender) and print("Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
