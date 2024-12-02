2bb63c42083d23a8c729c229dafbb49099006686
AnnetZh <anzhilkina@gmail.com>
2024-12-02 16:59:42 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print("Добро пожаловать, введите целые числа и произведите с ними математические действия сложения, вычитания, умножения или деления")
+a=int(input("Введите первое число"))
+b=int(input("Введите второе число"))
+
+operator=input("Какую математическу операцию будем производить? +,-,* или /?")
+try:
+    if operator == "+":
+    print(int(a)+int(b))
+    elif operator == "-":
+    print(int(a)-int(b))
+    elif operator == "*":
+    print(int(a)*int(b))
+    elif operator == "/":
+    print(int(a)/int(b))
+except ZeroDivisionError:
+    print("Произошло деление на ноль, в тру математике - это бесконечноть")
+else:
+print("Ошибка, проверьте введенные данные")
