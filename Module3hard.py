2ef369d99f51aa05a71cecfcf8b03f876067a3b7
AnnetZh <anzhilkina@gmail.com>
2024-12-10 20:10:51 +0300

    Add files via upload

--- Module3hard.py
+++ Module3hard.py
@@ -1,19 +1,26 @@
-###data_structure=[
-    #[1,2,3],
-    #{'a':4,'b':5},
-    #(6,{'cube':7,'drum':8}),
-    #"Hello",
-    #((),[{(2,'Urban',('Urban2',35))}])
-#]
+def calculate_structure_sum(data_structure):
+    if isinstance(data_structure, (int,float)):
+       return data_structure
+    if isinstance(data_structure, (str)):
+       return len(data_structure)
+    if len(data_structure)==0:
+       return 0
+    if isinstance(data_structure,(list,tuple,set)):
+        a=calculate_structure_sum(list(data_structure)[0])
+        b=calculate_structure_sum(list(data_structure)[1:])
+        return a+b
+    elif isinstance(data_structure,dict):
+        a=calculate_structure_sum(list(data_structure.keys()))
+        b=calculate_structure_sum(list(data_structure.values()))
+        return a+b
+    return 0
 
-def calculate_data_structure(data):
-    total_sum=0
-    if isinstance(data, int):
-    total_sum += data
-    elif isinstance(data, str):
-    total_sum += len(data)
-    #тут же теперь нужно по словарям и спискам пройтись, верно? но я ничего не нашла в лекциях этого и прошлого модуля про рекусивный вызов функции
-    return total_sum
-
+data_structure=[
+    [1,2,3],
+    {'a':4,'b':5},
+    (6,{'cube':7,'drum':8}),
+    "Hello",
+    ((),[{(2,'Urban',('Urban2',35))}])
+]
 result=calculate_structure_sum(data_structure)
 print(result)
