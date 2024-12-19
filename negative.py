d7df08f77c6037bf25f1b8f24c9f0ddc290e6df6
AnnetZh <anzhilkina@gmail.com>
2024-12-19 18:29:55 +0300

    Add files via upload

---
+++ negative.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+file=open("log.txt",'w')
+driver=webdriver.Chrome()
+option=webdriver.ChromeOptions()
+option.add_experimental_option('detach', True)
+#option.add_argument("--headless")
+driver=webdriver.Chrome(options=option)
+
+#Scenario functions---
+def set_up():
+    driver.get('http://www.saucedemo.com/')
+    driver.maximize_window()
+
+
+def login():
+    user_name=driver.find_element(By.XPATH,'//input[@id="user-name"]')
+    login='problem_user'
+    user_name.send_keys(login)
+    file.write("Successful login \n")
+
+    passw=driver.find_element(By.XPATH,'//input[@id="password"]')
+    password='secret_sauce'
+    passw.send_keys(password)
+    file.write("Successful password \n")
+
+    login_button=driver.find_element(By.XPATH,'//input[@id="login-button"]')
+    login_button.click()
+    file.write("Successful click login \n")
+
+def fake_login():
+    user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+    login = 'problem_user'
+    user_name.send_keys(login)
+    file.write("Successful login \n")
+
+    passw = driver.find_element(By.XPATH, '//input[@id="password"]')
+    password = 'secret_sauce11'
+    passw.send_keys(password)
+    file.write("Successful fake password \n")
+
+    login_button = driver.find_element(By.XPATH, '//input[@id="login-button"]')
+    login_button.click()
+    file.write("Successful click login \n")
+
+# End of Scenario functions
+
+# Tests ----
+
+def test_login_redirect():
+    correct_url='https://www.saucedemo.com/inventory.html'
+    get_url=driver.current_url
+
+    assert correct_url == get_url,"test_login_redirect is Failed"
+    file.write("test_login_redirect is success \n")
+
+def test_context_after_login_is_correct():
+    correct_text="Products"
+    current_text=driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
+    assert correct_text==current_text.text, "test_context_after_login_is_correct is Failed"
+    file.write("test_context_after_login_is_correct is success \n")
+
+def test_fake_login_label():
+    correct_text="Epic sadface: Username and password do not match any user in this service"
+    current_text=driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3')
+    assert correct_text==current_text.text, 'test_fake_login_label is Failed'
+    file.write("test_fake_login_label is successful \n")
+#End of tests
+
+def sc_real_login():
+    set_up()
+    login()
+    test_login_redirect()
+    test_context_after_login_is_correct()
+
+def sc_fake_login():
+    set_up()
+    fake_login()
+
+    test_fake_login_label()
+
+sc_fake_login()
+sc_real_login()
+file.close()
